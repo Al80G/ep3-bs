575ee272a95a49a43090e4c86ce6d0702ad39505
Al80G <alexgassner@live.at>
2026-04-14 18:06:56 +0200

    Translations

--- data/res/i18n-custom/de-DE/backend.php
+++ data/res/i18n-custom/de-DE/backend.php
@@ -9,4 +9,15 @@ return array(
     'Member' => 'Mitglied',
     'These are only editable for administration' => 'Diese sind nur für die Administration bearbeitbar',
 
+    'Open on weekdays (1=Mo, 2=Di, 3=Mi, 4=Do, 5=Fr, 6=Sa, 7=So)' => 'Geöffnet an Wochentagen (1=Mo, 2=Di, 3=Mi, 4=Do, 5=Fr, 6=Sa, 7=So)',
+    'Comma-separated, empty = all days' => 'Kommagetrennt, leer = alle Tage',
+
+    'Club Reserved Time (Start)' => 'Vereinsreservierung (Start)',
+    'Club Reserved Time (End)' => 'Vereinsreservierung (Ende)',
+    'Limits the reservation of club days from the `Behaviour` section to this time block.'
+        => 'Begrenzt die Vereinsreservierung aus dem Abschnitt `Verhalten` auf diesen Zeitblock.',
+
+    'Square control' => 'Platzkontrolle',
+    'Does a square control system exists for this aquare?' => 'Existiert ein Platzkontrollsystem für diesen Platz?',
+
 );
