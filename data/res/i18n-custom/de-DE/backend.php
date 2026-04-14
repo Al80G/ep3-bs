c9a6b3a465b901a6d33d3c84ed784f04c46eb5e8
Al80G <alexgassner@live.at>
2026-04-14 17:49:57 +0200

    Translations

---
+++ data/res/i18n-custom/de-DE/backend.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+    'Last activity: ' => 'Zuletzt aktiv: ',
+    'Last IP: ' => 'Letzte IP: ',
+    'Created: ' => 'Erstellt: ',
+
+    'Member' => 'Mitglied',
+    'These are only editable for administration' => 'Diese sind nur für die Administration bearbeitbar',
+
+);
