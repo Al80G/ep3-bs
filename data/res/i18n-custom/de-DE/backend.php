ced017f1d60fe1042f53d2a943bd27b328a33fcd
Al80G <alexgassner@live.at>
2026-04-14 18:09:42 +0200

    Translations

--- data/res/i18n-custom/de-DE/backend.php
+++ data/res/i18n-custom/de-DE/backend.php
@@ -9,6 +9,13 @@ return array(
     'Member' => 'Mitglied',
     'These are only editable for administration' => 'Diese sind nur für die Administration bearbeitbar',
 
+    'Reserve these days for the club' => 'Folgende Tage für den Verein reservieren',
+    'Day names (like Sunday) or concrete dates (like 2016-08-16);<br>Separated by line breaks or commas;<br>Force concrete dates to be free for everyone by adding a plus (like +2016-08-30)<br>The time can be additionally limited in the court config (i.e. 14:00 to 20:00)'
+        => 'Name des Tages (z. B. Sonntag) oder konkrete Datumangaben (z. B. 16.08.2016);<br>Getrennt durch Zeilenumbrüche oder Kommata;<br>Ein reservierter Tag kann mit einem Plus für alle freigegeben werden (z. B. +30.08.2016)<br>Die Zeit kann zusätzlich in der Platzkonfiguration eingeschränkt werden (z. B. 14:00 bis 20:00)',
+    'Display reserved time in Calendar' => 'Reservierte Zeit im Kalender anzeigen',
+    'activate to show the calendar cells as blocked for members - otherwise as free'
+        => 'Aktivieren, um die Kalenderfelder für Vereinstage als gesperrt anzuzeigen – sonst als frei',
+
     'Open on weekdays (1=Mo, 2=Di, 3=Mi, 4=Do, 5=Fr, 6=Sa, 7=So)' => 'Geöffnet an Wochentagen (1=Mo, 2=Di, 3=Mi, 4=Do, 5=Fr, 6=Sa, 7=So)',
     'Comma-separated, empty = all days' => 'Kommagetrennt, leer = alle Tage',
 
